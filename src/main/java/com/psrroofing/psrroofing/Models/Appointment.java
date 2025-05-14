ed8928f05e8db93d72dcdb77199b5ef708894730
RuelChauke <ruelchauke@investhoodit.co.za>
2025-05-14 11:24:19 +0200

    project

--- src/main/java/com/psrroofing/psrroofing/Models/Appointment.java
+++ src/main/java/com/psrroofing/psrroofing/Models/Appointment.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 public class Appointment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
